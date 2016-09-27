e712cba25bbb4f4cc214954c6e1392d4f0f6408f
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-09-27 21:34:58 +0200

    Ajout des fonctions "arbres"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # TP1 #
 #######
 
-# TODO : Arbres
+# TODO : Remplacer print des arbres par des logs + créer fonction loop
 # ____________________________________________________________________________________________________
 # Config
 
@@ -10,6 +10,8 @@
 import logging
 import sys
 import argparse
+import os
+import time
 
 # ____________________________________________________________________________________________________
 # ____________________________________________________________________________________________________
@@ -70,6 +72,85 @@ def afficheArgument():
     logging.info(
         ":\npath to the directory : %s \npath where to generate log : %s \ndepth of the directory : %s \nfrequency : %s hz\n",
         dp, lp, depth, frequence)
+
+
+# ___________________________________________________________________________________________________
+# Fonctions de creation de l'arbre du dossier et de comparaison
+
+def creatSurveyList(tree):
+    """
+    create a list of tupples form by (fileName, dateOfLastModif) corresponding to the files in the tree
+    """
+    listOfModifFiles = []
+    i = 0
+    while (i < len(tree)):
+        path, dirs, files = tree[i]
+        level = path.count(os.sep) - startinglevel
+        if (level <= depth_max):
+            print('### depth ', level, ' ### ', path, ' #####')
+            print("Sous dossiers : %s" % dirs)
+            print("Fichiers : %s" % files)
+            for file in files:
+                modifTime = os.path.getmtime(os.path.join(path, file))
+                listOfModifFiles += [(file, modifTime)]
+        i += 1
+    return (listOfModifFiles)
+
+
+def comparateSurveyList(oldListe, newListe):
+    """
+	args 2 lists which will be compared
+	return 3 lists :
+		- for the modified files
+		- for the added files
+		- for the deleted files
+	"""
+    if oldListe == newListe:
+        return None
+    else:
+        listOfSupprFiles = []
+        listOfAddFiles = []
+        listOfModifFiles = []
+        isCreated = [True] * len(newListe)
+        for o in oldListe:
+            isDeleted = True
+            for n in newListe:
+                nIndex = newListe.index(n)
+                oName, oTime = o
+                nName, nTime = n
+                if oName == nName:
+                    isDeleted = False
+                    isCreated[nIndex] = False
+                    if oTime != nTime:
+                        listOfModifFiles += [n]
+            if isDeleted:
+                listOfSupprFiles += [o]
+        for n in newListe:
+            nIndex = newListe.index(n)
+            if isCreated[nIndex]:
+                listOfAddFiles += [n]
+        return (listOfModifFiles, listOfAddFiles, listOfSupprFiles)
+
+
+def logTheMADLists(M, A, D):
+    """
+    	log the informations contained in the 3 lists :
+    		-M = modified files
+    		-A = added files
+    		-D = deleted files
+    """
+    if len(M):
+        print("M")
+        for (mFile, mTime) in M:
+            print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(mTime)), mFile, " is modified")
+    if len(A):
+        print("A")
+        for (aFile, aTime) in A:
+            print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(aTime)), aFile, " is added")
+    if len(D):
+        print("D")
+        for (dFile, dTime) in D:
+            print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(dTime)), dFile, " is deleted")
 
 
 # ___________________________________________________________________________________________________
